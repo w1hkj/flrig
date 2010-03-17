71273d121e9220449daaac222fa81278103a3877
David Freese <w1hkj@w1hkj.com>
2010-03-17 11:10:03 -0500

    IC746 / Pro Disassociation
        Removed several 746Pro commands from 746 class structure
        Modified 746 BW strings

--- src/include/ICgeneric.h
+++ src/include/ICgeneric.h
@@ -33,12 +33,9 @@ public:
 	long get_vfoA(void);
 	void set_vfoA(long);
 	int  get_smeter();
-	int  get_swr();
-	int  get_alc();
-	int  get_power_out();
-	int  get_power_control();
 	void set_volume_control(int val);
 	int  get_volume_control();
+	void get_vol_min_max_step(int &min, int &max, int &step);
 	void set_PTT_control(int val);
 	void set_noise(bool val);
 	int  get_noise();
@@ -46,7 +43,6 @@ public:
 	int  get_noise_reduction();
 	void set_noise_reduction_val(int val);
 	int  get_noise_reduction_val();
-	void tune_rig();
 	void set_attenuator(int val);
 	int  get_attenuator();
 	void set_preamp(int val);
@@ -54,22 +50,18 @@ public:
 	virtual void set_mode(int val);
 	virtual int  get_mode();
 	int  get_modetype(int n);
-	void set_bandwidth(int val);
-	int  get_bandwidth();
-	int  adjust_bandwidth(int m);
-	void set_notch(bool on, int val);
-	bool get_notch(int &val);
-	void get_notch_min_max_step(int &min, int &max, int &step);
+	virtual void set_bandwidth(int val);
+	virtual int  get_bandwidth();
 	void set_mic_gain(int val);
-	int  get_mic_gain();
+//	int  get_mic_gain();
 	void get_mic_gain_min_max_step(int &min, int &max, int &step);
 	void set_if_shift(int val);
-	bool get_if_shift(int val);
+//	bool get_if_shift(int val);
 	void get_if_min_max_step(int &min, int &max, int &step);
 	void set_squelch(int val);
-	int  get_squelch();
+//	int  get_squelch();
 	void set_rf_gain(int val);
-	int  get_rf_gain();
+//	int  get_rf_gain();
 	void set_power_control(double val);
 protected:
 	int  preamp_level;
@@ -84,9 +76,18 @@ public:
 	virtual ~RIG_IC746PRO(){}
 	void set_mode(int val);
 	int  get_mode();
-	int  adjust_bandwidth(int m);
+	void set_bandwidth(int val);
+	int  get_bandwidth();
+	int  get_swr();
+	int  get_alc();
+	int  get_power_out();
 	void set_attenuator(int val);
 	int  get_attenuator();
+	void tune_rig();
+	int  adjust_bandwidth(int m);
+	void set_notch(bool on, int val);
+	bool get_notch(int &val);
+	void get_notch_min_max_step(int &min, int &max, int &step);
 };
 
 class RIG_IC756PRO2 : public RIG_IC746PRO {
