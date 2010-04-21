195fcd9d36ebf7c790219ed0299f2b57fe7d3581
David Freese <w1hkj@w1hkj.com>
2010-04-21 07:50:40 -0500

    IC718
      Added Icom 718 transceiver

---
+++ src/rigs/IC718.cxx
@@ -0,0 +1,299 @@
+/*
+ * Icom IC-718
+ * 
+ * a part of flrig
+ * 
+ * Copyright 2009, Dave Freese, W1HKJ
+ * 
+ */
+
+#include "IC718.h"
+#include "debug.h"
+#include "rig.h"
+
+bool DEBUG_718 = true;
+
+//=============================================================================
+// IC-718
+//
+const char IC718name_[] = "IC-718";
+const char *IC718modes_[] = { "LSB", "USB", "AM", "CW", "RTTY", "CW-R", "RTTY-R", NULL};
+const char IC718_mode_type[] = { 'L', 'U', 'U', 'L', 'L', 'U', 'U'};
+//const char *IC718_widths[] = { "NARR", "WIDE", NULL};
+
+RIG_IC718::RIG_IC718() {
+	name_ = IC718name_;
+	modes_ = IC718modes_;
+	_mode_type = IC718_mode_type;
+
+//	bandwidths_ = IC718_widths;
+	comm_baudrate = BR9600;
+	stopbits = 2;
+	comm_retries = 2;
+	comm_wait = 5;
+	comm_timeout = 50;
+	comm_echo = true;
+	comm_rtscts = false;
+	comm_rtsplus = true;
+	comm_dtrplus = true;
+	comm_catptt = true;
+	comm_rtsptt = false;
+	comm_dtrptt = false;
+	mode_ = 1;
+	bw_ = 0;
+
+//	has_swr_control = true;
+	has_power_control = true;
+	has_volume_control = true;
+	has_mode_control = true;
+	has_bandwidth_control = true;
+	has_micgain_control = true;
+//	has_notch_control = true;
+	has_attenuator_control = true;
+	has_preamp_control = true;
+//	has_ifshift_control = true;
+//	has_ptt_control = true;
+//	has_tune_control = true;
+//	has_swr_control = true;
+	has_noise_control = true;
+	has_noise_reduction = true;
+	has_noise_reduction_control = true;
+//	has_alc_control = true;
+	has_rf_control = true;
+//	has_sql_control = true;
+
+	pre_to[2] = ok[3] = bad[3] = pre_fm[3] = 0x5E;
+};
+
+//=============================================================================
+
+long RIG_IC718::get_vfoA ()
+{
+	cmd = pre_to;
+	cmd += '\x03';
+	cmd.append( post );
+	if (!sendCommand(cmd, 11)) {
+		checkresponse(11);
+		return freq_;
+	}
+	freq_ = fm_bcd_be(&replystr[5], 10);
+	return freq_;
+}
+
+void RIG_IC718::set_vfoA (long freq)
+{
+	freq_ = freq;
+	cmd = pre_to;
+	cmd += '\x05';
+	cmd.append( to_bcd_be( freq, 10 ) );
+	cmd.append( post );
+	sendICcommand(cmd, 6);
+	checkresponse(6);
+}
+
+// Volume control val 0 ... 100
+void RIG_IC718::set_volume_control(int val)
+{
+	int ICvol = (int)(val * 2.55);
+	cmd = pre_to;
+	cmd.append("\x14\x01");
+	cmd.append(to_bcd(ICvol, 3));
+	cmd.append( post );
+	sendICcommand (cmd, 6);
+	checkresponse(6);
+	if (DEBUG_718)
+		LOG_INFO("%s", str2hex(cmd.data(), cmd.length()));
+}
+
+int RIG_IC718::get_volume_control()
+{
+	cmd = pre_to;
+	cmd.append("\x14\x01");
+	cmd.append( post );
+	if(sendICcommand (cmd, 9))
+		return ((int)(fm_bcd(&replystr[6],3) / 2.55));
+	checkresponse(9);
+	return 0;
+}
+
+void RIG_IC718::get_vol_min_max_step(int &min, int &max, int &step)
+{
+	min = 0; max = 100; step = 1;
+}
+
+int RIG_IC718::get_smeter()
+{
+	cmd = pre_to;
+	cmd.append("\x15\x02");
+	cmd.append( post );
+	if (sendICcommand (cmd, 9)) {
+		return fm_bcd(&replystr[6], 3) / 2.55;
+	} else
+		return 0;
+}
+
+void RIG_IC718::set_attenuator(int val)
+{
+	cmd = pre_to;
+	cmd += '\x11';
+	cmd += val ? '\x20' : '\x00';
+	cmd.append( post );
+	sendCommand (cmd, 6);
+	checkresponse(6);
+	if (DEBUG_718)
+		LOG_INFO("%s", str2hex(cmd.data(), cmd.length()));
+}
+
+void RIG_IC718::set_noise(bool val)
+{
+	cmd = pre_to;
+	cmd.append("\x16\x22");
+	cmd += val ? 1 : 0;
+	cmd.append(post);
+	sendICcommand(cmd, 6);
+	checkresponse(6);
+	if (DEBUG_718)
+		LOG_INFO("%s", str2hex(cmd.data(), cmd.length()));
+}
+
+int RIG_IC718::get_noise()
+{
+	cmd = pre_to;
+	cmd.append("\x16\x22");
+	cmd.append(post);
+	if (sendICcommand(cmd, 8))
+		return (replystr[6] ? 1 : 0);
+	return 0;
+}
+
+void RIG_IC718::set_noise_reduction(int val)
+{
+	cmd = pre_to;
+	cmd.append("\x16\x40");
+	cmd += val ? 1 : 0;
+	cmd.append(post);
+	sendICcommand(cmd, 6);
+	checkresponse(6);
+	if (DEBUG_718)
+		LOG_INFO("%s", str2hex(cmd.data(), cmd.length()));
+}
+
+int RIG_IC718::get_noise_reduction()
+{
+	cmd = pre_to;
+	cmd.append("\x16\x40");
+	cmd.append(post);
+	if (sendICcommand(cmd, 8))
+		return (replystr[6] ? 1 : 0);
+	return 0;
+}
+
+// 0 < val < 100
+void RIG_IC718::set_noise_reduction_val(int val)
+{
+	cmd = pre_to;
+	cmd.append("\x14\x06");
+	cmd.append(to_bcd(val * 2.55, 3));
+	cmd.append(post);
+	sendICcommand(cmd,6);
+	checkresponse(6);
+	if (DEBUG_718)
+		LOG_INFO("%s", str2hex(cmd.data(), cmd.length()));
+}
+
+int RIG_IC718::get_noise_reduction_val()
+{
+	cmd = pre_to;
+	cmd.append("\x14\x06");
+	cmd.append(post);
+	if(sendICcommand (cmd, 9))
+		return ((int)(fm_bcd(&replystr[6],3) / 2.55));
+	checkresponse(9);
+	return 0;
+}
+
+void RIG_IC718::set_preamp(int val)
+{
+	cmd = pre_to;
+	cmd += '\x16';
+	cmd += '\x02';
+	cmd += (unsigned char)val;
+	cmd.append( post );
+	sendICcommand (cmd, 6);
+	checkresponse(6);
+	if (DEBUG_718)
+		LOG_INFO("%s", str2hex(cmd.data(), cmd.length()));
+}
+
+int RIG_IC718::get_preamp()
+{
+	cmd = pre_to;
+	cmd += '\x16';
+	cmd += '\x02';
+	cmd.append( post );
+	if (sendICcommand (cmd, 8)) {
+		if (replystr[6] == 0x01) {
+			preamp_label("Pre", true);
+		} else {
+			preamp_label("Pre", false);
+		}
+	}
+	return replystr[6];
+}
+
+void RIG_IC718::set_rf_gain(int val)
+{
+	int ICrfg = (int)(val * 2.55);
+	cmd = pre_to;
+	cmd.append("\x14\x02");
+	cmd.append(to_bcd(ICrfg, 3));
+	cmd.append( post );
+	sendICcommand (cmd, 6);
+	checkresponse(6);
+	if (DEBUG_718)
+		LOG_INFO("%s", str2hex(cmd.data(), cmd.length()));
+}
+
+void RIG_IC718::set_squelch(int val)
+{
+	int ICsql = (int)(val * 2.55);
+	cmd = pre_to;
+	cmd.append("\x14\x03");
+	cmd.append(to_bcd(ICsql, 3));
+	cmd.append( post );
+	sendICcommand (cmd, 6);
+	checkresponse(6);
+	if (DEBUG_718)
+		LOG_INFO("%s", str2hex(cmd.data(), cmd.length()));
+}
+
+void RIG_IC718::set_power_control(double val)
+{
+	cmd = pre_to;
+	cmd.append("\x14\x0A");
+	cmd.append(to_bcd((int)(val * 2.55), 3));
+	cmd.append( post );
+	sendICcommand (cmd, 6);
+	checkresponse(6);
+	if (DEBUG_718)
+		LOG_INFO("%s", str2hex(cmd.data(), cmd.length()));
+}
+
+void RIG_IC718::set_mic_gain(int val)
+{
+	val = (int)(val * 2.55);
+	cmd = pre_to;
+	cmd.append("\x14\x0B");
+	cmd.append(to_bcd(val,3));
+	cmd.append(post);
+	sendICcommand(cmd, 6);
+	checkresponse(6);
+	if (DEBUG_718)
+		LOG_INFO("%s", str2hex(cmd.data(), cmd.length()));
+}
+
+int RIG_IC718::get_modetype(int n)
+{
+	return _mode_type[n];
+}
+
