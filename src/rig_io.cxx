w1hkj/flrig

commit d57238d61f4714a5ce9f3f37969799242da4a261

IC703 bug fixes
  Config dialog not properly initialized for IC703 type
  Echo suppression algorithm corrected

src/rig_io.cxx
@@ -148,20 +148,27 @@ int sendCommand (string s, int retnbr, bool b)
 					(9 + progStatus.stopbits) * 1000.0 / RigSerial.Baud());
 
 	if (RigSerial.IsOpen() == false) {
-		LOG_DEBUG("command: %s", b ? str2hex(s.data(), s.length()) : s.c_str());
+		LOG_INFO("command: %s", b ? str2hex(s.data(), s.length()) : s.c_str());
 		return 0;
 	}
 
-	LOG_DEBUG("sent %s", b ? str2hex(s.data(), s.length()) : s.c_str());
+	if (RIG_DEBUG)
+		LOG_INFO("out:%3d, %s", s.length(), b ? str2hex(s.data(), s.length()) : s.c_str());
 
 	RigSerial.WriteBuffer(s.c_str(), numwrite);
 	MilliSleep( readafter );
 	numread = readResponse();
-	LOG_DEBUG("reply %s", b ? str2hex(replybuff, numread) : replybuff);
+
+	if (RIG_DEBUG)
+		LOG_INFO("in :%3d, %s", numread, b ? str2hex(replybuff, numread) : replybuff);
+
 	if (numread > retnbr) {
-		memmove(replybuff, replybuff + numread - retnbr, retnbr+1);
+		memmove(replybuff, replybuff + numread - retnbr - 1, retnbr);
 		numread = retnbr;
 	}
+
+	if (RIG_DEBUG)
+		LOG_INFO("rep:%3d, %s", numread, b ? str2hex(replybuff, numread) : replybuff);
 
 	replystr.clear();
 	for (int i = 0; i < numread; replystr += replybuff[i++]);

src/dialogs.cxx
@@ -249,7 +249,6 @@ void cbCIVdefault()
 	txtCIV->value(hexstr);
 	progStatus.CIV = srig->defaultCIV;
 	srig->adjustCIV(progStatus.CIV);
-printf("CIV %2X\n", srig->CIV);
 }
 
 void cbCIV()
@@ -258,10 +257,8 @@ void cbCIV()
 	int adr = 0;
 	rigbase *srig = rigs[picked];
 	sscanf(txtCIV->value(), "0x%2X", &adr);
-//	progStatus.CIV = atol(txtCIV->value());
 	progStatus.CIV = adr;
 	srig->adjustCIV(progStatus.CIV);
-printf("CIV %2X\n", srig->CIV);
 }
 
 void cbUSBaudio()
@@ -377,7 +374,7 @@ void configXcvr()
 	selectAuxPort->value(progStatus.aux_serial_port.c_str());
 	selectSepPTTPort->value(progStatus.sep_serial_port.c_str());
 
-	if (rig_nbr >= IC706MKIIG && rig_nbr <= IC910H) {
+	if (rig_nbr >= IC703 && rig_nbr <= IC910H) {
 		char hexstr[8];
 		snprintf(hexstr, sizeof(hexstr), "0x%2X", selrig->CIV);
 		txtCIV->value(hexstr);

src/rigs/IC703.cxx
@@ -55,7 +55,7 @@ long RIG_IC703::get_vfoA ()
 	cmd = pre_to;
 	cmd += '\x03';
 	cmd.append( post );
-	if (!sendCommand(cmd, 11)) {
+	if (!sendICcommand(cmd, 11)) {
 		checkresponse(11);
 		return freq_;
 	}
@@ -72,8 +72,6 @@ void RIG_IC703::set_vfoA (long freq)
 	cmd.append( post );
 	sendICcommand(cmd, 6);
 	checkresponse(6);
-	if (RIG_DEBUG)
-		LOG_INFO("%s", str2hex(cmd.data(), cmd.length()));
 }
 
 void RIG_IC703::set_mode(int val)
@@ -86,8 +84,6 @@ void RIG_IC703::set_mode(int val)
 	cmd.append( post );
 	sendICcommand (cmd, 6);
 	checkresponse(6);
-	if (RIG_DEBUG)
-		LOG_INFO("%s", str2hex(cmd.data(), cmd.length()));
 }
 
 int RIG_IC703::get_mode()

src/support.cxx
@@ -1468,6 +1468,25 @@ void initRig()
 	opBW->index( vfoA.iBW );
 	selrig->set_bandwidth(vfoA.iBW);
 
+	if (selrig->CIV) {
+		char hexstr[8];
+		snprintf(hexstr, sizeof(hexstr), "0x%2X", selrig->CIV);
+		txtCIV->value(hexstr);
+		txtCIV->activate();
+		btnCIVdefault->activate();
+		if (strstr(selrig->name_, "IC-7200") || strstr(selrig->name_, "IC-7600")) {
+			btnUSBaudio->value(progStatus.USBaudio = false);
+			btnUSBaudio->activate();
+		} else
+			btnUSBaudio->deactivate();
+	} else {
+		txtCIV->value("");
+		txtCIV->deactivate();
+		btnCIVdefault->deactivate();
+		btnUSBaudio->value(false);
+		btnUSBaudio->deactivate();
+	}
+
 	// enable the serial thread
 	pthread_mutex_unlock(&mutex_serial);
 
@@ -1535,7 +1554,7 @@ void initStatusConfigDialog()
 	rig_nbr = progStatus.rig_nbr;
 	selrig = rigs[rig_nbr];
 
-	if (rig_nbr >= IC706MKIIG && rig_nbr <= IC910H)
+	if (rig_nbr >= IC703 && rig_nbr <= IC910H)
 		if (progStatus.CIV) selrig->adjustCIV(progStatus.CIV);
 
 	selectRig->index(rig_nbr);

commit 6e82feff3219c21b78b8e4f07a63fe6073e5aeaa

Echo suppression
  Correct error introduced by d57238d6

src/rig_io.cxx
@@ -163,7 +163,7 @@ int sendCommand (string s, int retnbr, bool b)
 		LOG_INFO("in :%3d, %s", numread, b ? str2hex(replybuff, numread) : replybuff);
 
 	if (numread > retnbr) {
-		memmove(replybuff, replybuff + numread - retnbr - 1, retnbr);
+		memmove(replybuff, replybuff + numread - retnbr, retnbr);
 		numread = retnbr;
 	}
 