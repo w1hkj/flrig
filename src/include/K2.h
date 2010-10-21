3d64fc011b3abdd06b8ead69e99bc72eeaf3c225
David Freese <w1hkj@w1hkj.com>
2010-10-20 20:48:26 -0500

    K2 Update
      K2 update submitted by Ken Haase, VE4KEH

--- src/include/K2.h
+++ src/include/K2.h
@@ -8,26 +8,27 @@ public:
 	RIG_K2();
 	~RIG_K2(){}
 
+	void initialize();
 	long get_vfoA();
 	void set_vfoA(long);
 	void set_attenuator(int val);
 	int  get_attenuator();
 	void set_preamp(int val);
 	int  get_preamp();
-/*
-	int  get_smeter();
-	int  get_swr();
-	int  get_power_out();
-	int  get_power_control();
-	void set_volume_control(double val);
-	void set_power_control(double val);
-	void set_PTT_control(int val);
-	void tune_rig();
 	void set_mode(int val);
 	int  get_mode();
-	int  get_modetype();
+	int  get_modetype(int n);
 	void set_bandwidth(int val);
-*/
+	int get_bandwidth();
+	void tune_rig();
+	void set_PTT_control(int val);
+    virtual void get_pc_min_max_step(int &min, int &max, int &step){
+        min = 0; max = 110; step = 1; }
+	int  get_power_control();
+	void set_power_control(double val);
+	int  get_smeter();
+	int  get_power_out();
+
 };
 
 #endif
