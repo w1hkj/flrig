adbdf641cd9b7d8ed754c34d0db552673746160a
David Freese <w1hkj@w1hkj.com>
2011-01-12 10:35:41 -0600

    Orion-II TT566
      Added TT565/566 class

---
+++ src/include/TT566.h
@@ -0,0 +1,47 @@
+#ifndef _TT566_H
+#define _TT566_H
+
+#include "ICbase.h"
+
+class RIG_TT566 : public rigbase {
+private:
+	int fwdpwr;
+	int refpwr;
+
+public:
+	RIG_TT566();
+	~RIG_TT566(){}
+
+	long get_vfoA();
+	void set_vfoA(long);
+	long get_vfoB();
+	void set_vfoB(long);
+	void set_modeA(int val);
+	int  get_modeA();
+	void set_modeB(int val);
+	int  get_modeB();
+	void set_bwA(int bw);
+	int  get_bwA();
+	void set_bwB(int bw);
+	int  get_bwB();
+
+	void set_PTT_control(int val);
+	int  get_modetype(int n);
+	void set_preamp(int);
+	int  get_preamp();
+	int  get_power_control(void);
+	void set_power_control(double val);
+	void set_auto_notch(int v);
+	int  get_auto_notch();
+	void set_attenuator(int val);
+	int  get_attenuator();
+	int  get_smeter();
+	int  get_power_out();
+	int  get_swr();
+	
+
+
+//	void set_agc_level();
+};
+
+#endif
