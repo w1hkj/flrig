77ea58ade395dd29247fb26d26916391b536a64e
David Freese <w1hkj@w1hkj.com>
2011-02-04 19:10:44 -0600

    TS590S
      Added back end for Kenwood TS-590S transceiver

---
+++ src/include/TS590S.h
@@ -0,0 +1,75 @@
+#ifndef _TS590S_H
+#define _TS590S_H
+
+#include "rigbase.h"
+
+class RIG_TS590S : public rigbase {
+private:
+	bool notch_on;
+	int  preamp_level;
+	int  att_level;
+	bool data_mode;
+	int  active_mode;
+	int  active_bandwidth;
+public:
+	RIG_TS590S();
+	~RIG_TS590S(){}
+	
+	void initialize();
+
+	long get_vfoA();
+	void set_vfoA(long);
+	long get_vfoB();
+	void set_vfoB(long);
+
+	int  get_modetype(int n);
+	void set_modeA(int val);
+	int  get_modeA();
+	void set_modeB(int val);
+	int  get_modeB();
+
+	int  adjust_bandwidth(int val);
+	void set_widths();
+	void set_active_bandwidth();
+	int  get_active_bandwidth();
+	void set_bwA(int val);
+	int  get_bwA();
+	void set_bwB(int val);
+	int  get_bwB();
+
+	bool can_split() { return false;}
+	void set_split(bool val) {split = val;}
+	bool twovfos() {return true;}
+
+	int  get_smeter();
+	int  get_swr();
+	int  get_power_out();
+	int  get_power_control();
+	void set_volume_control(int val);
+	int  get_volume_control();
+	void set_power_control(double val);
+	void set_PTT_control(int val);
+	void tune_rig();
+	void set_attenuator(int val);
+	int  get_attenuator();
+	void set_preamp(int val);
+	int  get_preamp();
+
+	void set_if_shift(int val);
+	bool get_if_shift(int &val);
+	void get_if_min_max_step(int &min, int &max, int &step);
+	void set_notch(bool on, int val);
+	bool get_notch(int &val);
+	void get_notch_min_max_step(int &min, int &max, int &step);
+	void set_noise(bool b);
+	void set_mic_gain(int val);
+	int  get_mic_gain();
+	void get_mic_min_max_step(int &min, int &max, int &step);
+
+	bool sendTScommand(string, int, bool, bool);
+	const char **bwtable(int);
+
+};
+
+
+#endif
