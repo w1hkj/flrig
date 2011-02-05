e74673dfd9670450d5c501f26ad475fa1af0c7fd
David Freese <w1hkj@w1hkj.com>
2011-02-04 19:04:31 -0600

    FT1000MP mods
      Changes to FT1000mp back end

--- src/include/FT1000MP.h
+++ src/include/FT1000MP.h
@@ -8,18 +8,40 @@ class RIG_FT1000MP : public rigbase {
 public :
 	RIG_FT1000MP();
 	~RIG_FT1000MP() {};
+
+	void initialize();
+
+	bool can_split() { return true;}
+	void set_split(bool val);
+	bool twovfos() {return true;}
+
 	long get_vfoA();
 	void set_vfoA(long);
-	void set_PTT_control(int val);
+	long get_vfoB(void);
+	void set_vfoB(long f);
+
 	void set_modeA(int val);
 	int  get_modeA();
+	void set_modeB(int val);
+	int  get_modeB();
 	int  get_modetype(int n);
+
+	void selectA();
+	void selectB();
+
 	int  get_bwA();
 	void set_bwA(int n);
+	int  get_bwB();
+	void set_bwB(int n);
+
+	void set_PTT_control(int val);
+	void tune_rig();
+
 	int  get_power_out(void);
 	int  get_smeter(void);
 
 private:
+	bool tune_on;
 	void init_cmd();
 };
 
