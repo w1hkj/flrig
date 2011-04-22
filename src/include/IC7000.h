a1b425e9ab4410e4275751f2603ffb40df0822c6
David Freese <w1hkj@w1hkj.com>
2011-04-22 15:33:28 -0500

    IC7000
      * Added virtual methods to supplant 746pro methods
      * Added new event log reporting
      * Removed filter selection from set_mode_... methods

--- src/include/IC7000.h
+++ src/include/IC7000.h
@@ -8,10 +8,24 @@ public:
 	RIG_IC7000();
 	~RIG_IC7000(){}
 
-	void set_modeA(int val);
+	void initialize();
+
+	long get_vfoA(void);
+	void set_vfoA(long f);
+	int  get_bwA();
+	void set_bwA(int);
 	int  get_modeA();
+	void set_modeA(int val);
+
+	long get_vfoB(void);
+	void set_vfoB(long f);
+	int  get_bwB();
+	void set_bwB(int);
 	void set_modeB(int val);
 	int  get_modeB();
+
+	void selectA();
+	void selectB();
 
 	int  adjust_bandwidth(int m);
 	int  def_bandwidth(int m);
@@ -21,6 +35,7 @@ public:
 	int  get_preamp();
 
 	void set_auto_notch(int v);
+	void set_split(bool v);
 
 };
 
