e1748e535869d4bce8665251489e51a07e4d5e09
David Kjellquist <dave@kjellquist.com>
2011-04-24 15:03:43 -0500

    TS570
      * Added vfo B - freq, mode, bw
      * Added noise blanker
      * Added Split
      * Added showresp event logging

--- src/include/TS570.h
+++ src/include/TS570.h
@@ -17,8 +17,29 @@ public:
 
 	bool get_ts570id();
 
+	void selectA();
+	void selectB();
+	void set_split(bool val);
+
 	long get_vfoA();
 	void set_vfoA(long);
+	long get_vfoB();
+	void set_vfoB (long);
+
+	void set_modeA(int val);
+	int  get_modeA();
+	void set_modeB(int val);
+	int  get_modeB();
+	int  get_modetype(int n);
+
+	void set_bwA(int val);
+	int  get_bwA();
+	void set_bwB(int val);
+	int  get_bwB();
+	int  adjust_bandwidth(int val);
+	int  def_bandwidth(int val);
+	void set_widths();
+
 	int  get_smeter();
 	int  get_swr();
 	int  get_power_out();
@@ -32,22 +53,17 @@ public:
 	int  get_attenuator();
 	void set_preamp(int val);
 	int  get_preamp();
-	void set_modeA(int val);
-	int  get_modeA();
-	int  get_modetype(int n);
-	void set_bwA(int val);
-	int  get_bwA();
-	int  adjust_bandwidth(int val);
-	int  def_bandwidth(int val);
-	void set_widths();
+
 	//	void set_if_shift(int val);
 	//	bool get_if_shift(int &val);
 	//	void get_if_min_max_step(int &min, int &max, int &step);
 	//	void set_notch(bool on, int val);
 	//	bool get_notch(int &val);
 	//	void get_notch_min_max_step(int &min, int &max, int &step);
-	void set_beatcancel(int val);
-	int  get_beatcancel();
+
+	void set_noise(bool b);
+	int  get_noise();
+
 	void set_mic_gain(int val);
 	int  get_mic_gain();
 	void get_mic_min_max_step(int &min, int &max, int &step);
