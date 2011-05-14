fe162af44f6bbdd13a79c1f0453ca018adc23e2e
David Freese <w1hkj@w1hkj.com>
2011-05-13 21:38:17 -0500

    K3 revert
      Revert K3 class design to b4f2298a6d8ab state

--- src/include/K3_ui.h
+++ src/include/K3_ui.h
@@ -5,9 +5,7 @@
 //
 // =====================================================================
 
-void read_K3_rx();
-void read_K3_tx();
-void parse_K3();
+void read_K3();
 void K3_set_split(int val);
 void K3_A2B();
 void cb_K3_swapAB();
