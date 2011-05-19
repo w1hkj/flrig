40dce513b5b0f40c8f80811c9a70f3aedc998034
David Freese <w1hkj@w1hkj.com>
2011-05-19 12:40:08 -0500

    TT588 Orion-II
      * Added control methods
        - control is currently only for the primary receiver
      * Added event logging

--- src/include/TT566.h
+++ src/include/TT566.h
@@ -7,6 +7,7 @@ class RIG_TT566 : public rigbase {
 private:
 	int fwdpwr;
 	int refpwr;
+	int atten_level;
 
 public:
 	RIG_TT566();
@@ -26,15 +27,21 @@ public:
 	int  get_bwB();
 
 	void set_PTT_control(int val);
+
 	int  get_modetype(int n);
 	void set_preamp(int);
 	int  get_preamp();
+	void set_attenuator(int val);
+	int  get_attenuator();
+
+	void set_noise(bool);
+	int  get_noise();
+
 	int  get_power_control(void);
 	void set_power_control(double val);
 	void set_auto_notch(int v);
 	int  get_auto_notch();
-	void set_attenuator(int val);
-	int  get_attenuator();
+
 	int  get_smeter();
 	int  get_power_out();
 	int  get_swr();
@@ -42,6 +49,9 @@ public:
 	void get_vol_min_max_step(int &min, int &max, int &step) {
 		min = 0; max = 100; step = 1; }
 	void set_volume_control(int val);
+
+	void set_mic_gain(int val);
+	int  get_mic_gain();
 
 //	void set_if_shift(int val);
 //	bool get_if_shift(int &val);
@@ -54,6 +64,7 @@ public:
 		min = 0; max = 100; step = 1; }
 
 	int  adjust_bandwidth(int m);
+	int  def_bandwidth(int m) {return adjust_bandwidth(m);}
 
 //	void set_agc_level();
 };
