1871f9cc40b9c7efd2dd58f08ae9763a7c8c3f57
David Freese <w1hkj@w1hkj.com>
2013-02-20 15:58:17 -0600

    FT950
      * Added VFO adjust control

--- src/UI/ui_small.cxx
+++ src/UI/ui_small.cxx
@@ -1090,7 +1090,7 @@ Fl_Double_Window* Small_rig_window() {
 		genericMisc = new Fl_Group(2, 271, 420, 49, _("Misc"));
 			genericMisc->hide();
 
-			cnt_vfo_adj = new Fl_Counter(24, 279, 96, 22, _("Vfo Adj(ppm)"));
+			cnt_vfo_adj = new Fl_Counter(24, 279, 96, 22, _("Vfo Adj"));
 			cnt_vfo_adj->type(1);
 			cnt_vfo_adj->callback((Fl_Callback*)cb_cnt_vfo_adj);
 			cnt_vfo_adj->align(Fl_Align(34));
