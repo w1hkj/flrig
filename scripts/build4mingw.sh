5992ef4868c2827688605a7902dc8e76a220f3c2
David Freese <w1hkj@w1hkj.com>
2013-06-20 10:52:42 -0500

    MXE build
      * mods to correctly build on Mxe-mingw32

--- scripts/build4mingw.sh
+++ scripts/build4mingw.sh
@@ -2,9 +2,21 @@
 
 ### Script to build a win32 installation
 
-./configure $CROSSCFG $PKGCFG FLTK_CONFIG=$PREFIX/bin/fltk-config --with-ptw32=$PREFIX/ptw32 XMLRPC_C_CONFIG=$PREFIX/bin/xmlrpc-c-config
+./configure \
+  $PKGCFG \
+  $CROSSCFG \
+  --without-asciidoc \
+  --with-ptw32=/opt/mxe/usr/i686-pc-mingw32 \
+  --with-libiconv-prefix=$PREFIX/iconv \
+  --enable-static \
+  --with-libintl-prefix=$PREFIX/gettext \
+  PTW32_LIBS="-lpthread -lpcreposix -lpcre -lregex" \
+  FLTK_CONFIG=$PREFIX/bin/i686-pc-mingw32-fltk-config \
+
 make
-i586-mingw32msvc-strip src/flrig.exe
+
+make
+$PREFIX/bin/i686-pc-mingw32-strip src/flrig.exe
 make nsisinst
 mv src/*setup*exe .
 
