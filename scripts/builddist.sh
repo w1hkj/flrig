c53c0a8f8d3f0fe57825320fedd8de424f479f48
David Freese <w1hkj@w1hkj.com>
2013-06-20 10:52:42 -0500

    MXE build
      * mods to correctly build on Mxe-mingw32

--- scripts/builddist.sh
+++ scripts/builddist.sh
@@ -1,15 +1,27 @@
 # build file to generate the distribution binary tarball
+myscripts/cleanup
 
-make clean
+autoreconf
+
 ./configure --prefix=/tmp/flrig-build --enable-static
 make install-strip
 tar czf flrig-$1.bin.tgz -C /tmp/flrig-build .
 
 make clean
 
-./configure $CROSSCFG $PKGCFG FLTK_CONFIG=$PREFIX/bin/fltk-config --with-ptw32=$PREFIX/ptw32 XMLRPC_C_CONFIG=$PREFIX/bin/xmlrpc-c-config
+./configure \
+  $PKGCFG \
+  $CROSSCFG \
+  --without-asciidoc \
+  --with-ptw32=/opt/mxe/usr/i686-pc-mingw32 \
+  --with-libiconv-prefix=$PREFIX/iconv \
+  --enable-static \
+  --with-libintl-prefix=$PREFIX/gettext \
+  PTW32_LIBS="-lpthread -lpcreposix -lpcre -lregex" \
+  FLTK_CONFIG=$PREFIX/bin/i686-pc-mingw32-fltk-config \
+
 make
-i586-mingw32msvc-strip src/flrig.exe
+$PREFIX/bin/i686-pc-mingw32-strip src/flrig.exe
 make nsisinst
 mv src/*setup*exe .
 
