41a0e82bcaf6571ce48b874cc66de598fa58b193
David Freese <w1hkj@w1hkj.com>
2014-05-24 07:34:44 -0500

    update build scripts

--- scripts/build4mingw.sh
+++ scripts/build4mingw.sh
@@ -1,23 +1,18 @@
 #!/bin/sh
 
-### Script to build a win32 installation
+# build windows installation file
 
 ./configure \
   $PKGCFG \
   $CROSSCFG \
-  --without-asciidoc \
   --with-ptw32=/opt/mxe/usr/i686-pc-mingw32 \
   --with-libiconv-prefix=$PREFIX/iconv \
   --enable-static \
-  --with-libintl-prefix=$PREFIX/gettext \
   PTW32_LIBS="-lpthread -lpcreposix -lpcre -lregex" \
-  FLTK_CONFIG=$PREFIX/bin/i686-pc-mingw32-fltk-config \
-
-make
+  FLTK_CONFIG=$PREFIX/bin/i686-pc-mingw32-fltk-config
 
 make
 $PREFIX/bin/i686-pc-mingw32-strip src/flrig.exe
 make nsisinst
 mv src/*setup*exe .
-
 
--- scripts/builddist.sh
+++ scripts/builddist.sh
@@ -1,24 +1,22 @@
 # build file to generate the distribution binary tarball
-myscripts/cleanup
-
 autoreconf
 
+# build linux binary
 ./configure --prefix=/tmp/flrig-build --enable-static
 make install-strip
 tar czf flrig-$1.bin.tgz -C /tmp/flrig-build .
 
 make clean
 
+# build windows installation file
 ./configure \
   $PKGCFG \
   $CROSSCFG \
-  --without-asciidoc \
   --with-ptw32=/opt/mxe/usr/i686-pc-mingw32 \
   --with-libiconv-prefix=$PREFIX/iconv \
   --enable-static \
-  --with-libintl-prefix=$PREFIX/gettext \
   PTW32_LIBS="-lpthread -lpcreposix -lpcre -lregex" \
-  FLTK_CONFIG=$PREFIX/bin/i686-pc-mingw32-fltk-config \
+  FLTK_CONFIG=$PREFIX/bin/i686-pc-mingw32-fltk-config
 
 make
 $PREFIX/bin/i686-pc-mingw32-strip src/flrig.exe
