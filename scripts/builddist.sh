76d12b0fa60c0fd85b7796d78c6dcbafd9bfb312
David Freese <iam_w1hkj@w1hkj.com>
2015-08-14 13:53:19 -0500

    MXE update

--- scripts/builddist.sh
+++ scripts/builddist.sh
@@ -5,14 +5,14 @@ autoreconf
 ./configure \
   $PKGCFG \
   $CROSSCFG \
-  --with-ptw32=/opt/mxe/usr/i686-pc-mingw32 \
+  --with-ptw32=$PREFIX/usr/i686-w64-mingw32.static \
   --with-libiconv-prefix=$PREFIX/iconv \
   --enable-static \
   PTW32_LIBS="-lpthread -lpcreposix -lpcre -lregex" \
-  FLTK_CONFIG=$PREFIX/bin/i686-pc-mingw32-fltk-config
+  FLTK_CONFIG=$PREFIX/bin/i686-w64-mingw32.static-fltk-config
 
 make
-$PREFIX/bin/i686-pc-mingw32-strip src/flrig.exe
+$PREFIX/bin/i686-w64-mingw32.static-strip src/flrig.exe
 make nsisinst
 mv src/*setup*exe .
 
--- scripts/config4mingw.sh
+++
@@ -1,8 +0,0 @@
-./configure \
-  $PKGCFG \
-  $CROSSCFG \
-  --with-ptw32=/opt/mxe/usr/i686-pc-mingw32 \
-  --with-libiconv-prefix=$PREFIX/iconv \
-  PTW32_LIBS="-lpthread -lpcreposix -lpcre -lregex" \
-  FLTK_CONFIG=$PREFIX/bin/i686-pc-mingw32-fltk-config
-
