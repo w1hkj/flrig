d6abfff75a0a339d51193088e6a0a3f1a2d8fcef
Dan Winker <github.dwinker@spamgourmet.com>
2018-03-27 13:11:07 -0500

    Change num to n. num is number of transmit+receive bytes used to compute timeout. n is the number of bytes we expect to receive.

--- src/rigs/ICbase.cxx
+++ src/rigs/ICbase.cxx
@@ -224,7 +224,7 @@ civ.close();
 		for ( cnt = 0; cnt < delay; cnt++) {
 			readResponse();
 			returned.append(replystr);
-			if (returned.length() >= num) {
+			if (returned.length() >= n) {
 				replystr = returned;
 				unsigned long int waited = zmsec() - tod_start;
 				snprintf(sztemp, sizeof(sztemp), "%s ans in %ld ms, OK  ", sz, waited);
