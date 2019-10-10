ce82f3f04033d87dfe8f9db9a11cbd76a8d541f2
David Freese <w1hkj@bellsouth.net>
2019-10-10 08:10:07 -0500

    Unguarded statement
      * fix unguarded statement in src/TS990.cxx

--- src/rigs/TS990.cxx
+++ src/rigs/TS990.cxx
@@ -2799,10 +2799,11 @@ int  RIG_TS990::get_rf_gain()
 		if (wait_char(';', 7, TS990_WAIT, "get rf gain", ASC) < 7) return val;
 
 		size_t p = replystr.rfind("RG");
-		if (p != string::npos)
+		if (p != string::npos) {
 			val = fm_decimal(replystr.substr(p+3), 3);
 			val *= 100;
 			val /= 255;
+		}
 	}
 	return val;
 }
