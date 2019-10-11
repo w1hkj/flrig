ad39fdab175b48be3779134b8ddbb5d42fb47615
David Freese <w1hkj@bellsouth.net>
2019-10-11 12:13:07 -0500

    scripts
      * modify build scripts for current OS install

--- scripts/builddist.sh
+++ scripts/builddist.sh
@@ -4,14 +4,14 @@ autoreconf
 # build windows installation file
 ./configure \
   $PKGCFG \
-  $CROSSCFG \
+  --host=i686-w64-mingw32.static \
   --with-ptw32=$PREFIX/i686-w64-mingw32.static \
   --with-libiconv-prefix=$PREFIX/iconv \
   --enable-static \
   PTW32_LIBS="-lpthread -lpcreposix -lpcre -lregex" \
   FLTK_CONFIG=$PREFIX/i686-w64-mingw32.static/bin/fltk-config
 
-make
+make -j 3
 $PREFIX/bin/i686-w64-mingw32.static-strip src/flrig.exe
 make nsisinst
 mv src/*setup*exe .
